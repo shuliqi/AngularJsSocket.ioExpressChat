6f5ce893a02a1b1245896cb51083e6db3a1eafd3
shuliqi <1169046371@qq.com>
2016-04-06 15:54:18 +0800

    完成

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,117 +1,149 @@
+var app = angular.module("chatRoom",[]);
+// 定义一个（依赖注入）服务,socket是对socket.io的封装，这里采用的是factory服务，一共有三种创建服务的方式,factory,service,privider
+app.factory("socket",function($rootScope){//这是angular的根元素
+	var socket = io();                    //默认连接部署网站的服务器
+	return{                               //返回一个对象
+			on:function(even,callback){
+				socket.on(event,function(){
+					var arguments = arguments;
+					$rootScope.$apply(socket,arguments);
+				});
+			},
+			emit:function(event,data,callback){
+				socket.emit(event,data,function(){
+					var arguments = arguments;
+					$rootScope.$apply(function(){
+						if (callback) {
+							callback.apply(socket,arguments);
+						}
+					});
+				});
+			}
+		};
+});
+//定义一个依赖注入（服务）randomColor,用来给用户添加一个头像的颜色的
+app.factory("randomColor",function(){
+	return{
+		color:function(){
+			return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).slice(-6);
+		}
+	}；
+})
+// 定义一个userService服务，主要是用来判断当前的用户名在当前在线下是否已经使用
+app.factory("userService",function(){
+	return{
+		getName:function(users,name){
+			if (users instanceof Array) {
+				for (var i = 0; i < users.length; i++) {
+					 if (users[i].name ==== name ) {
+					 	return users[i];
+					 }
+				}
+			}else{
+				return null;
+			}
 
-var app=angular.module("chatRoom",[]);
-app.factory('socket', function($rootScope) {
-    var socket = io(); //默认连接部署网站的服务器
-    return {
-        on: function(eventName, callback) {
-            socket.on(eventName, function() {
-                var args = arguments;
-                $rootScope.$apply(function() {   //手动执行脏检查
-                    callback.apply(socket, args);
-                });
-            });
-        },
-        emit: function(eventName, data, callback) {
-            socket.emit(eventName, data, function() {
-                var args = arguments;
-                $rootScope.$apply(function() {
-                    if(callback) {
-                        callback.apply(socket, args);
-                    }
-                });
-            });
+		}
+	}
+})
+// 定义一个控制器，用来控制整个流程
+app.controller("chatCtrl",["$scope","socket","randomColor","userService"],function($scope,socket,randomColor,userService){
+	$scope.logined = false;          //使用angular的show和hide，他们的判断会根据logined,false表示不显示该部分内容，true表示显示该内容
+	$scope.receiver = "";            //因为有两种聊法。群聊和私聊。默认情况下是群聊
+	$scope.publicMessages= []        //群聊的信息存放在这里
+	$scope.privateMessages= []       //私聊的的信息存放在这里
+    $scope.messages = $scope.publicMessages;          //默认显示的消息是群聊的信息
+    $scope.users = [];                                //存放在线的用户信息
+    $scope.color = randomColor.color();               //在线的用户的头像颜色
+    $scope.login = function(){                        //用户触发这个事件
+    	socket.emit("addUser",{name:$scope.name,color:$scope.color});     //socket服务的emit发出一个userAdd事件给服务器  获取html的name值和在factory的color值
+    }
+    // 监听服务器传进来的用户昵称判断是否已经使用的结果
+    socket.on("userAddResult",function(data){
+        if (data.result) {
+            $scope.userExisted = false;                //用户昵称没有被占用
+            $scope.logined = true;                     //让登录部分隐藏
+        }else{
+            $scope.userExisted = true; 
         }
-    };
-});
-app.factory('randomColor', function($rootScope) {
-    return {
-        newColor: function() {
-            return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).slice(-6);
+    });
+    //html在写完信息点击发送之后 会执行这个事件 ，会获取ng-moduole的words值
+    $scope.postmsg = function(){
+    	// 一条消息的信息
+    	var msg = {text:$scope.words,type:"normal",color:$scope.color,from:$scope.name,to:$scope.receiver};
+    	var rec = $scope.receiver;
+    	// 私聊的情况
+    	if (rec){
+    		if (!$scope.privateMessages[rec]) {
+    			$scope.privateMessages[rec] = [];
+    		}
+    		$scope.privateMessages[rec].push(msg);    //把群聊信息保存在privateMessages数组里
+    	}
+    	// 群聊的情况
+    	else{
+    		$scope.publicMessages[rec].push(msg);    //把群聊信息保存在publicMessages数组里
+    	}
+ 		$scope.words = "";
+        if (rec !== $scope.name) {                   // 排除给自己发的状况
+        	socket.emit("addMessage",msg);          //发出一个messagesAdd事件
         }
-    };
-});
-app.factory('userService', function($rootScope) {
-    return {
-        get: function(users,nickname) {
-            if(users instanceof Array){
-                for(var i=0;i<users.length;i++){
-                    if(users[i].nickname===nickname){
-                        return users[i];
-                    }
-                }
+    } 
+    $scope.setreceiver=function(receiver){
+    	$scope.receiver = receiver;
+    	// 私信部分
+    	if (receiver) {
+    		if (!$scope.privateMessages[receiver]) {
+    			$scope.privateMessages[receiver] = [];
+    		}
+    		$scope.messages = $scope.privateMessages[receiver];
+    	}
+    	// 群发的
+    	else{
+    		$scope.messages = $scope.publicMessages[receiver];
+    	}
+    	var user = userService.getName($scope.users,receiver)
+    	if (users) {
+    		users.logined = false;
+    	}
+    }
+
+    //监听用户发来的userAdded事件
+    socket.on("userAdded",function(data){
+    	if (!$scope.logined) return ;
+    	$scope.publicMessages.push({text:data.name,type:"welcom"});
+    	$scope.users.push(data);
+    })
+    socket.on("allUser",function(){
+    	if (!$scope.logined) return ;
+    	$scope.users = data;
+    })
+    //消息发送之后，有私发和群发
+    socket.on("mesaageAdded",function(data){
+        if (!$scope.logined) return;
+        // 判断是否是私发
+        if (data.to) {
+            if (!$scope.privateMessages[data.form]) {
+                $scope.privateMessages[data.form] = [];
+            }
+            // 保存发送者的消息
+            $scope.privateMessages[data.form].push(data);
+        }else{//群发状态
+            $scope.publicMessages.push(data);
+        }
+        // 定义一个发送者，通过定义的factory服务获取
+        var fromUser = userService.get($scope.users,data.from);
+        // 定义接受者
+        var toUser = userService.get($scope.users,data.from);
+        // 判断提示有多少未读信息 这个条件是不在私聊状态中
+        if ($scope.receiver !== data.to) {
+            if (fromUser && toUser.name) {
+                fromUser.hasNewMessage = true;
             }else{
-                return null;
+                fromUser.hasNewMessage = false;
             }
         }
-    };
-});
-app.controller("chatCtrl",['$scope','socket','randomColor','userService',function($scope,socket,randomColor,userService){
-    // var messageWrapper= $('.message-wrapper');
-    $scope.hasLogined=false;
-    $scope.receiver="";//默认是群聊
-    $scope.publicMessages=[];//群聊消息
-    $scope.privateMessages={};//私信消息
-    $scope.messages=$scope.publicMessages;//默认显示群聊
-    $scope.users=[];//
-    $scope.color=randomColor.newColor();//当前用户头像颜色
-    $scope.login=function(){   //登录进入聊天室
-        socket.emit("addUser",{nickname:$scope.nickname,color:$scope.color});
-    }
-    $scope.scrollToBottom=function(){
-        messageWrapper.scrollTop(messageWrapper[0].scrollHeight);
-    }
-    $scope.postMessage=function(){
-        var msg={text:$scope.words,type:"normal",color:$scope.color,from:$scope.nickname,to:$scope.receiver};
-        var rec=$scope.receiver;
-        if(rec){  //私信
-           if(!$scope.privateMessages[rec]){
-               $scope.privateMessages[rec]=[];
-           }
-            $scope.privateMessages[rec].push(msg);
-        }else{ //群聊
-            $scope.publicMessages.push(msg);
-        }
-        $scope.words="";
-        if(rec!==$scope.nickname) { //排除给自己发的情况
-            socket.emit("addMessage", msg);
-        }
-    }
-    $scope.setReceiver=function(receiver){
-        $scope.receiver=receiver;
-        if(receiver){ //私信用户aa
-            if(!$scope.privateMessages[receiver]){
-                $scope.privateMessages[receiver]=[];
-            }
-            $scope.messages=$scope.privateMessages[receiver];
-        }else{//广播
-            $scope.messages=$scope.publicMessages;
-        }
-        var user=userService.get($scope.users,receiver);
-        if(user){
-            user.hasNewMessage=false;
-        }
-    }
-    //收到登录结果
-    socket.on('userAddingResult',function(data){
-        if(data.result){
-            $scope.userExisted=false;
-            $scope.hasLogined=true;
-        }else{//昵称被占用
-            $scope.userExisted=true;
-        }
     });
-    //接收到欢迎新用户消息
-    socket.on('userAdded', function(data) {
-        if(!$scope.hasLogined) return;
-        $scope.publicMessages.push({text:data.nickname,type:"welcome"});
-        $scope.users.push(data);
-    });
-    //接收到在线用户消息
-    socket.on('allUser', function(data) {
-        if(!$scope.hasLogined) return;
-        $scope.users=data;
-    });
-    //接收到用户退出消息
+    // 用户退出聊天室
     socket.on('userRemoved', function(data) {
         if(!$scope.hasLogined) return;
         $scope.publicMessages.push({text:data.nickname,type:"bye"});
@@ -122,28 +154,9 @@ app.controller("chatCtrl",['$scope','socket','randomColor','userService',functio
             }
         }
     });
-    //接收到新消息
-    socket.on('messageAdded', function(data) {
-        if(!$scope.hasLogined) return;
-        if(data.to){ //私信
-            if(!$scope.privateMessages[data.from]){
-                $scope.privateMessages[data.from]=[];
-            }
-            $scope.privateMessages[data.from].push(data);
-        }else{//群发
-            $scope.publicMessages.push(data);
-        }
-        var fromUser=userService.get($scope.users,data.from);
-        var toUser=userService.get($scope.users,data.to);
-        if($scope.receiver!==data.to) {//与来信方不是正在聊天当中才提示新消息
-            if (fromUser && toUser.nickname) {
-                fromUser.hasNewMessage = true;//私信
-            } else {
-                toUser.hasNewMessage = true;//群发
-            }
-        }
-    });
 }]);
+
+//自定义两个指令,用来渲染uers和message页面
 app.directive('message', ['$timeout',function($timeout) {
     return {
         restrict: 'E',
@@ -162,19 +175,20 @@ app.directive('message', ['$timeout',function($timeout) {
         }
     };
 }])
-    .directive('user', ['$timeout',function($timeout) {
-        return {
-            restrict: 'E',
-            templateUrl: 'user.html',
-            scope:{
-                info:"=",
-                iscurrentreceiver:"=",
-                setreceiver:"&"
-            },
-            link:function(scope, elem, attrs,chatCtrl){
-                $timeout(function(){
-                    elem.find('.avatar').css('background',scope.info.color);
-                });
-            }
-        };
-    }]);
+.directive('user', ['$timeout',function($timeout) {
+    return {
+        restrict: 'E',
+        templateUrl: 'user.html',
+        scope:{
+            info:"=",
+            iscurrentreceiver:"=",
+            setreceiver:"&"
+        },
+        link:function(scope, elem, attrs,chatCtrl){
+            $timeout(function(){
+                elem.find('.avatar').css('background',scope.info.color);
+            });
+        }
+    };
+}]);
+
